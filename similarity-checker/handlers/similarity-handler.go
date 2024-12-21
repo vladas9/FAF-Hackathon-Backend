97a64f3be1655ea27b0b3f1a2d446848f6d4ee2f
vladas9 <vladamza63@gmail.com>
2024-12-22 01:34:28 +0200

    smilarity

---
+++ similarity-checker/handlers/similarity-handler.go
@@ -0,0 +1,142 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/jpoz/groq"
+)
+
+type Message struct {
+	Url string `json:"url"`
+}
+
+func HandleGetSimilarity(c *gin.Context) {
+	msg := &Message{}
+	if err := c.ShouldBindJSON(msg); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	content := calculateSimilarity()
+
+	c.JSON(http.StatusOK, content)
+}
+
+func calculateSimilarity() string {
+	// func main() {
+	godotenv.Load()
+
+	text1, err := getContent("https://www.mirror.co.uk/news/politics/donald-trump-become-president-timeline-34059687")
+	text2, err := getContent("https://www.bbc.com/news/articles/cde7ng85jwgo")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	client := groq.NewClient()
+	var groqResponse *groq.ChatCompletion
+
+	maxRetries := 3
+	for attempts := 1; attempts <= maxRetries; attempts++ {
+		groqResponse, err = client.CreateChatCompletion(groq.CompletionCreateParams{
+			Model:          "gemma2-9b-it",
+			ResponseFormat: groq.ResponseFormat{Type: "json_object"},
+			Messages: []groq.Message{
+				{
+					Role:    "system",
+					Content: "Always respond with valid JSON only. Do not include any additional text or explanation.",
+				},
+				{
+					Role:    "user",
+					Content: "Your task is to compare two news articles and provide a similarity score for each meaningful part based on the following criteria: Identify relevant sentences or segments that contain 10 or more words, Focus on comparing the factual content and information from both articles, Look for contradictions (e.g., conflicting facts), redundancies, and note how similar the ideas are in terms of meaning and context, Assign a similarity score between 0 and 100 for each part, categorizing them as 'Exact Match', 'Partial Match', or 'Contradiction'. If the articles are entirely unrelated, mark them as 'Not Related' with a similarity score of 0%,If the articles discuss different topics without significant overlap, indicate 'Not Related'. Please also identify similar ideas or phrasing, even if the wording differs.",
+				},
+				{
+					Role: "user",
+					Content: "Your answer should be in the following format:\n" +
+						"{\n" +
+						"    overallScore: ..., \n" +
+						"    similarParts: [\n" +
+						"        { part1: 'Text from Article 1', part2: 'Text from Article 2', score: 'similarity score', typeOfSimilarity: 'Exact Match' | 'Partial Match' | 'Contradiction' },\n" +
+						"        ...\n" +
+						"    ],\n" +
+						"    conflictingParts: [\n" +
+						"        { part1: 'Text from Article 1', part2: 'Text from Article 2', score: 'similarity score', typeOfSimilarity: 'Exact Match' | 'Partial Match' | 'Contradiction' },\n" +
+						"        ...\n" +
+						"    ],\n" +
+						"    notRelated: true // Indicate if the articles are unrelated, with a similarity score of 0%.\n" +
+						"}",
+				},
+				{
+					Role:    "user",
+					Content: fmt.Sprintf("Text1: %s, Text2: %s", text1, text2),
+				},
+			},
+		})
+
+		if err != nil {
+			log.Printf("Error on attempt %d: %v", attempts, err)
+		} else if groqResponse == nil || len(groqResponse.Choices) == 0 || len(groqResponse.Choices[0].Message.Content) == 0 {
+			log.Printf("Empty response on attempt %d", attempts)
+		} else {
+			fmt.Println(groqResponse.Choices[0].Message.Content)
+			return groqResponse.Choices[0].Message.Content
+		}
+
+		if attempts < maxRetries {
+			log.Printf("Retrying... attempt %d", attempts+1)
+			time.Sleep(2 * time.Second)
+		}
+	}
+	return ""
+}
+
+type PageContent struct {
+	Paragraphs string   `json:"paragraphs"`
+	Headings   []string `json:"headings"`
+	Images     []string `json:"images"`
+	Lists      []string `json:"lists"`
+	Spans      []string `json:"spans"`
+}
+
+func getContent(url string) (*PageContent, error) {
+	apiURL := "http://localhost:8000/get-content"
+	jsonData := fmt.Sprintf("{\"url\": \"%s\"}", url)
+
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer([]byte(jsonData)))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make API request: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	var apiResponse *PageContent
+	err = json.Unmarshal(body, &apiResponse)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse JSON response: %v", err)
+	}
+
+	return apiResponse, nil
+}
+
+func getUrls(url string) {}
